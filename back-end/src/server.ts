5a903e8118d790633ad6b9e28ec03502714f0c84
Nika Gabriadze <nickgabriadze@gmail.com>
2023-08-18 22:45:37 +0400

    Implemented server-side functionality in order to fetch the necessary access tokens for the user to be able to view his/her account information(details, playlists, songs, tracks and so on)

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,11 +1,89 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import queryString from "query-string";
+import { v4 } from "uuid";
+import axios from "axios";
+
+dotenv.config();
+
+const CLIENT_ID: string = process.env.VITE_SPOTIFY_CLIENT_ID
+  ? process.env.VITE_SPOTIFY_CLIENT_ID
+  : "";
+const CLIENT_SECRET: string = process.env.VITE_SPOTIFY_CLIENT_SECRET
+  ? process.env.VITE_SPOTIFY_CLIENT_SECRET
+  : "";
+const redirect_uri = "http://localhost:3001/callback";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+app.get("/authenticate", (req, res) => {
+  const scope =
+    "ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email user-read-private";
+  const state = v4();
+  res.send(
+    "https://accounts.spotify.com/authorize?" +
+      queryString.stringify({
+        response_type: "code",
+        client_id: CLIENT_ID,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
+  );
+});
 
-app.listen(3001, () => [
-    console.log("Running on port 3001")
-])
+app.get("/callback", async (req, res) => {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+
+  if (state === null) {
+    res.redirect(
+      "/#" +
+        queryString.stringify({
+          error: "state_mismatch",
+        })
+    );
+  } else {
+    try {
+      const response = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        null,
+        {
+          params: {
+            code: code,
+            redirect_uri: redirect_uri,
+            grant_type: "authorization_code",
+          },
+          headers: {
+            Authorization:
+              "Basic " +
+              Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
+          },
+        }
+      );
+
+      const acccess = response.data;
+      console.log(acccess);
+      // You can now send the accessToken to your frontend
+      res.redirect(
+        "http://localhost:5173/#" +
+          queryString.stringify({
+            accessToken: acccess.access_token,
+            token_type: "Bearer",
+            expires_in: 3600,
+            refresh_token: acccess.refresh_token,
+          })
+      );
+    } catch (error) {
+      // Handle errors appropriately
+      res.status(500).send("Error occurred");
+    }
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Running on port 3001");
+});
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,8 +4,7 @@ import { useAppDispatch, useAppSelector } from "./store/hooks";
 import appStyle from "./app.module.css";
 import Search from "./components/search/search";
 import Navigation from "./components/navigation/navigation";
-
-
+import axiosInstance from "./axios";
 
 export function App() {
   const dispatch = useAppDispatch();
@@ -15,12 +14,31 @@ export function App() {
 
   const homeOrSearch = useAppSelector((state) => state.navigationReducer.navTo);
 
+  if (!window.location.hash.includes("#")) {
+    axiosInstance
+      .get("http://localhost:3001/authenticate")
+      .then((res) => (window.location.href = res.data));
+  } else {
+    const text = window.location.hash
+      .split("#")[1]
+      .split("&")
+      .map((each) => {
+        const split = each.split("=");
+       
+        const key = split[0];
+        const value = split[1];
+        const obj =JSON.parse(`{"${key}":"${value}"}`)
+       
+        return obj
+        
+      });
+    console.log(text);
+  }
 
   return (
-    <div className={appStyle['application-wrapper']}>
+    <div className={appStyle["application-wrapper"]}>
       <Navigation />
-      {homeOrSearch==='search'?  <Search />: ''}
-      
+      {homeOrSearch === "search" ? <Search /> : ""}
     </div>
   );
 }
