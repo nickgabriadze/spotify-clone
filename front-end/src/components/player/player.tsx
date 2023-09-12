734bc5379cd48f3716e3b830c21f46d69044d5a0
Nika Gabriadze <nickgabriadze@gmail.com>
2023-09-12 12:54:28 +0400

    Trying to save the previously played song, because there is no way to fetch that data from spotify, because 'Currently Playing song is not retrieve-able' after long periods of inactivity .(It has a small issue, but the idea matters the most).

--- front-end/src/components/player/player.tsx
+++ front-end/src/components/player/player.tsx
@@ -1,138 +1,158 @@
-import { useState, useEffect, useCallback } from "react";
+import {useState, useEffect, useCallback} from "react";
 import playerStyle from "./player.module.css";
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { getCurrentlyPlaying } from "../../api/player/getCurrentlyPlaying";
-import { CurrentlyPlaying } from "../../types/currentlyPlaying";
-import { getDevices } from "../../api/player/getDevices";
-import { Devices } from "../../types/device";
+import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import {getCurrentlyPlaying} from "../../api/player/getCurrentlyPlaying";
+import {CurrentlyPlaying} from "../../types/currentlyPlaying";
+import {getDevices} from "../../api/player/getDevices";
+import {Devices} from "../../types/device";
 import SongDetails from "./playerComponents/SongsDetails";
 import DeviceController from "./playerComponents/DeviceController";
 import StreamController from "./playerComponents/StreamController";
-import { setCurrentlyPlayingSong, setUserControlActions } from "../../store/features/navigationSlice";
+import {setCurrentlyPlayingSong, setUserControlActions} from "../../store/features/navigationSlice";
 import GraphEQ from "./icons/graphicEq.svg"
 
 export function Player() {
-  const [currentlyPlaying, setCurrentlyPlaying] = useState<CurrentlyPlaying>();
-  const [currentLoading, setCurrentLoading] = useState<boolean>(true);
-  const [noDataAvailable, setNoDataAvailable] = useState(true);
-  const [devices, setDevices] = useState<Devices>();
-  const [error, setError] = useState<string | unknown>();
-  const access = useAppSelector((state) => state.spotiUserReducer.spotiToken);
-  const dispatch = useAppDispatch();
-  const userActions = useAppSelector(state => state.navigationReducer.userControlActions);
+    const [currentlyPlaying, setCurrentlyPlaying] = useState<CurrentlyPlaying>();
+    const [currentLoading, setCurrentLoading] = useState<boolean>(true);
+    const [noDataAvailable, setNoDataAvailable] = useState(true);
+    const [devices, setDevices] = useState<Devices>();
+    const [error, setError] = useState<string | unknown>();
+    const access = useAppSelector((state) => state.spotiUserReducer.spotiToken);
+    const dispatch = useAppDispatch();
+    const userActions = useAppSelector(state => state.navigationReducer.userControlActions);
 
 
+    const fetchCurrentData = useCallback(async () => {
+        try {
+            setCurrentLoading(true)
+            const devices = await getDevices(access.accessToken);
+            const devicesData = devices.data;
 
-  const fetchCurrentData = useCallback( async () => {
-    try {
-      setCurrentLoading(true)
-      const devices = await getDevices(access.accessToken);
-      const devicesData = devices.data;
+            setDevices(devicesData);
+            const req = await getCurrentlyPlaying(access.accessToken);
+            const data = req.data;
+            if (req.status === 204) {
+                setNoDataAvailable(true);
+               const prevData = window.localStorage.getItem('previousSong')
+                if(prevData) {
+                    const currentlyPlayingSong:{
+                        artistID: string,
+                        songID: string,
+                        albumID: string,
+                        isPlaying: boolean | null
+                    } = JSON.parse(prevData);
 
-      setDevices(devicesData);
-      const req = await getCurrentlyPlaying(access.accessToken);
-      const data = req.data;
-      if (req.status === 204) {
-        setNoDataAvailable(true);
-      } else {
-        setCurrentlyPlaying(data);
-        setNoDataAvailable(false);
-        
-        dispatch(setCurrentlyPlayingSong({
-          currentlyPlayingSong: {
-            artistID: data.item.artists[0].id,
-            albumID: data.item.album.id,
-            songID: data.item.id,
-            isPlaying: data.is_playing
-          }
-        }))
-      }
-    } catch (err) {
-      setError(err);
-    } finally {
-      setCurrentLoading(false);
-    }
-  }, [access.accessToken, dispatch]);
+                    dispatch(setCurrentlyPlayingSong({
+                        currentlyPlayingSong
+                    }))
+                }
+            } else {
+                setCurrentlyPlaying(data);
+                setNoDataAvailable(false);
 
-  useEffect(() => {
-        fetchCurrentData();
-        if (userActions.length > 50) {
-          dispatch(setUserControlActions({
-            userAction: 'Nullify'
-          }))
-        } else {
-          fetchCurrentData();
+                window.localStorage.setItem('previousSong', JSON.stringify({
+                    artistID: data.item.artists[0].id,
+                    albumID: data.item.album.id,
+                    songID: data.item.id,
+                    isPlaying: data.is_playing
+                }))
 
-        }
-
-      },
-      [fetchCurrentData, dispatch, userActions.length])
-
-  
-  useEffect(() => {
-    const fetchCurrent = async () => {
-      try {
-       
-        const devices = await getDevices(access.accessToken);
-        const devicesData = devices.data;
-
-        setDevices(devicesData);
-        const req = await getCurrentlyPlaying(access.accessToken);
-        const data = req.data;
-        if (req.status === 204) {
-          setNoDataAvailable(true);
-        } else {
-          setCurrentlyPlaying(data);
-          setNoDataAvailable(false);
-          
-          dispatch(setCurrentlyPlayingSong({
-            currentlyPlayingSong: {
-              artistID: data.item.artists[0].id,
-              albumID: data.item.album.id,
-              songID: data.item.id,
-              isPlaying: data.is_playing
+                dispatch(setCurrentlyPlayingSong({
+                    currentlyPlayingSong: {
+                        artistID: data.item.artists[0].id,
+                        albumID: data.item.album.id,
+                        songID: data.item.id,
+                        isPlaying: data.is_playing
+                    }
+                }))
             }
-          }))
-       
+        } catch (err) {
+            setError(err);
+        } finally {
+            setCurrentLoading(false);
         }
-      } catch (err) {
-        setError(err);
-      } 
-    };
+    }, [access.accessToken, dispatch]);
 
-    
-    const fetcher = setInterval(() => fetchCurrent(), 3000);
+    useEffect(() => {
+            fetchCurrentData();
+            if (userActions.length > 50) {
+                dispatch(setUserControlActions({
+                    userAction: 'Nullify'
+                }))
+            } else {
+                fetchCurrentData();
 
-    return () => clearInterval(fetcher)
-  
+            }
 
-  }, [access, dispatch]);
+        },
+        [fetchCurrentData, dispatch, userActions.length])
 
 
+    useEffect(() => {
+        const fetchCurrent = async () => {
+            try {
 
-  if (noDataAvailable) {
-    document.title = "Spotify Clone";
-    return <div></div>;
-  } else {
-    document.title = currentlyPlaying?.item?.name?  String(currentlyPlaying?.item?.name)
-      .concat(" • ")
-      .concat(String(currentlyPlaying?.item?.artists.map((each) => each.name).join(", "))) : 'Spotify Clone';
+                const devices = await getDevices(access.accessToken);
+                const devicesData = devices.data;
 
-    return (
-      <section className={playerStyle["player"]}>
-        <div className={playerStyle['player-wrapper']}>
-        <SongDetails  currentlyPlaying={noDataAvailable ? undefined: currentlyPlaying} />
-        <StreamController accessToken={access.accessToken} currentlyPlaying={noDataAvailable ? undefined: currentlyPlaying}/>
-        <DeviceController devices={noDataAvailable ? undefined: devices} />
-        </div>
-        <div className={playerStyle['which-device']}>
-            <img
-                alt={"Small Graphical Eq icon"}
-                src={GraphEQ} width={25} height={25}></img><h5>Listening on {devices?.devices.filter((each) => each.is_active)[0]?.name}</h5>
-        </div>
-      </section>
-    );
-  }
+                setDevices(devicesData);
+                const req = await getCurrentlyPlaying(access.accessToken);
+                const data = req.data;
+                if (req.status === 204) {
+                    setNoDataAvailable(true);
+                } else {
+                    setCurrentlyPlaying(data);
+                    setNoDataAvailable(false);
+
+                    dispatch(setCurrentlyPlayingSong({
+                        currentlyPlayingSong: {
+                            artistID: data.item.artists[0].id,
+                            albumID: data.item.album.id,
+                            songID: data.item.id,
+                            isPlaying: data.is_playing
+                        }
+                    }))
+
+                }
+            } catch (err) {
+                setError(err);
+            }
+        };
+
+
+        const fetcher = setInterval(() => fetchCurrent(), 3000);
+
+        return () => clearInterval(fetcher)
+
+
+    }, [access, dispatch]);
+
+
+    if (noDataAvailable) {
+        document.title = "Spotify Clone";
+        return <div></div>;
+    } else {
+        document.title = currentlyPlaying?.item?.name ? String(currentlyPlaying?.item?.name)
+            .concat(" • ")
+            .concat(String(currentlyPlaying?.item?.artists.map((each) => each.name).join(", "))) : 'Spotify Clone';
+
+        return (
+            <section className={playerStyle["player"]}>
+                <div className={playerStyle['player-wrapper']}>
+                    <SongDetails currentlyPlaying={noDataAvailable ? undefined : currentlyPlaying}/>
+                    <StreamController accessToken={access.accessToken}
+                                      currentlyPlaying={noDataAvailable ? undefined : currentlyPlaying}/>
+                    <DeviceController devices={noDataAvailable ? undefined : devices}/>
+                </div>
+                <div className={playerStyle['which-device']}>
+                    <img
+                        alt={"Small Graphical Eq icon"}
+                        src={GraphEQ} width={25} height={25}></img><h5>Listening
+                    on {devices?.devices.filter((each) => each.is_active)[0]?.name}</h5>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Player;
