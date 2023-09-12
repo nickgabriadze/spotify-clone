41ee7ac627830437c46d81275ea5ea2bd6cd9e38
Nika Gabriadze <nickgabriadze@gmail.com>
2023-09-12 17:09:36 +0400

    Done with the top result and top songs. Made the design smoother and also added skeletons for both.

--- front-end/src/components/search/components/each-search-component/All/innerComponents/Songs.tsx
+++ front-end/src/components/search/components/each-search-component/All/innerComponents/Songs.tsx
@@ -12,91 +12,90 @@ import Play from "../icons/play.svg";
 import episodesStyle from "../../PodcastsShows/podcastsShows.module.css";
 import TopSongCardSkeleton from "../../../../../../skeletons/topSongCardSkeleton.tsx";
 
-export function Songs({firstFour, resultsLoading}: { firstFour: Track[] | undefined, resultsLoading:boolean}) {
+export function Songs({firstFour, resultsLoading}: { firstFour: Track[] | undefined, resultsLoading: boolean }) {
     const [hoveringOver, setHoveringOver] = useState<string>('none');
     const currentlyPlaying = useAppSelector((state) => state.navigationReducer.currentlyPlayingSong);
     const accessToken = useAppSelector(state => state.spotiUserReducer.spotiToken.accessToken);
     const dispatch = useAppDispatch();
 
 
-
-
-
     return <div className={allResultsStyle['top-songs-wrapper']}>
         <h2>Songs</h2>
         <div className={allResultsStyle['top-songs-box']}>
-            {firstFour?.map((eachTrack, i) => {
-                if(resultsLoading){
-               return (<div key={i} className={allResultsStyle['top-song']}
-                     onClick={async () => {
+            {resultsLoading ?
+                Array.from({length: 4}).map((_, i) => <TopSongCardSkeleton key={i}/>)
+                :
 
-                         if (!(currentlyPlaying.songID === String(eachTrack?.id))) {
-                             await PlayResumeStreaming(accessToken, undefined, [String(eachTrack?.uri)]);
-                             dispatch(
-                                 setUserControlActions({
-                                     userAction: "Play Track",
-                                 })
-                             );
-                         } else if (currentlyPlaying.isPlaying && currentlyPlaying.songID === String(eachTrack?.id)) {
-                             await PauseStreaming(accessToken);
-                             dispatch(
-                                 setUserControlActions({
-                                     userAction: "Pause Track",
-                                 })
-                             );
-                         } else if (!(currentlyPlaying.isPlaying) && currentlyPlaying.songID === String(eachTrack?.id)) {
-                             await PlayResumeStreaming(accessToken);
-                             dispatch(
-                                 setUserControlActions({
-                                     userAction: "Resume Track",
-                                 })
-                             );
-                         }
-                     }}
-                     style={eachTrack.id === hoveringOver ? {
-                         cursor: "pointer",
-                         backgroundColor: `#3b3a3a`,
-                         borderRadius: '4px'
-                     } : {}}
-                     onMouseOut={() => setHoveringOver('none')}
-                     onMouseOver={() => setHoveringOver(eachTrack.id)}>
-                    <div className={allResultsStyle['song-img-details']}>
-                        <div className={allResultsStyle['interactive-album-img']}>
-                            <img alt={'Album image'} src={eachTrack?.album.images[0]?.url} width={40} height={40}></img>
-                            {eachTrack.id === hoveringOver &&
-                                <div>{(currentlyPlaying.songID === String(eachTrack?.id) && currentlyPlaying.isPlaying) ?
-                                    <img style={{padding: '5px'}} className={allResultsStyle['play-button']} width={40}
-                                         alt={'Pause Button'} height={40} src={Pause}></img>
-                                    :
-                                    <img className={allResultsStyle['play-button']} width={40} height={40}
-                                         alt={'Play Button'} src={Play}></img>
-                                }
+                firstFour?.map((eachTrack, i) =>
+                    <div key={i} className={allResultsStyle['top-song']}
+                         onClick={async () => {
 
-                                </div>}
-                        </div>
-                        <div className={allResultsStyle['song-details']}>
-                            <a>{eachTrack?.name[0].toUpperCase().concat(eachTrack?.name.slice(1,))}</a>
-                            <div className={allResultsStyle['song-artists']}>
-                                {eachTrack?.explicit ?
-                                    <div className={episodesStyle['explicit']} style={{fontSize: '8px'}}>E</div> : ''}
-                                {eachTrack.artists.map((artist, i) =>
-                                    <a>{i === eachTrack.artists.length - 1 ? artist.name : `${artist.name}, `}</a>)}
+                             if (!(currentlyPlaying.songID === String(eachTrack?.id))) {
+                                 await PlayResumeStreaming(accessToken, undefined, [String(eachTrack?.uri)]);
+                                 dispatch(
+                                     setUserControlActions({
+                                         userAction: "Play Track",
+                                     })
+                                 );
+                             } else if (currentlyPlaying.isPlaying && currentlyPlaying.songID === String(eachTrack?.id)) {
+                                 await PauseStreaming(accessToken);
+                                 dispatch(
+                                     setUserControlActions({
+                                         userAction: "Pause Track",
+                                     })
+                                 );
+                             } else if (!(currentlyPlaying.isPlaying) && currentlyPlaying.songID === String(eachTrack?.id)) {
+                                 await PlayResumeStreaming(accessToken);
+                                 dispatch(
+                                     setUserControlActions({
+                                         userAction: "Resume Track",
+                                     })
+                                 );
+                             }
+                         }}
+                         style={eachTrack.id === hoveringOver ? {
+                             cursor: "pointer",
+                             backgroundColor: `#3b3a3a`,
+                             borderRadius: '4px'
+                         } : {}}
+                         onMouseOut={() => setHoveringOver('none')}
+                         onMouseOver={() => setHoveringOver(eachTrack.id)}>
+                        <div className={allResultsStyle['song-img-details']}>
+                            <div className={allResultsStyle['interactive-album-img']}>
+                                <img alt={'Album image'} src={eachTrack?.album.images[0]?.url} width={40}
+                                     height={40}></img>
+                                {eachTrack.id === hoveringOver &&
+                                    <div>{(currentlyPlaying.songID === String(eachTrack?.id) && currentlyPlaying.isPlaying) ?
+                                        <img style={{padding: '5px'}} className={allResultsStyle['play-button']}
+                                             width={40}
+                                             alt={'Pause Button'} height={40} src={Pause}></img>
+                                        :
+                                        <img className={allResultsStyle['play-button']} width={40} height={40}
+                                             alt={'Play Button'} src={Play}></img>
+                                    }
+
+                                    </div>}
+                            </div>
+                            <div className={allResultsStyle['song-details']}>
+                                <a>{eachTrack?.name[0].toUpperCase().concat(eachTrack?.name.slice(1,))}</a>
+                                <div className={allResultsStyle['song-artists']}>
+                                    {eachTrack?.explicit ?
+                                        <div className={episodesStyle['explicit']}
+                                             style={{fontSize: '8px'}}>E</div> : ''}
+                                    {eachTrack.artists.map((artist, i) =>
+                                        <a key={i}>{i === eachTrack.artists.length - 1 ? artist.name : `${artist.name}, `}</a>)}
+                                </div>
                             </div>
                         </div>
+                        <div className={allResultsStyle['song-duration-heart']}>
+                            {eachTrack.id === hoveringOver &&
+                                <div><img alt={"Heart icon"} src={HeartSVG} width={20} height={28}></img></div>}
+                            <div
+                                className={allResultsStyle['duration']}>{millisecondsToHhMmSs(Number(eachTrack?.duration_ms))}</div>
+                        </div>
                     </div>
-                    <div className={allResultsStyle['song-duration-heart']}>
-                        {eachTrack.id === hoveringOver &&
-                            <div><img alt={"Heart icon"} src={HeartSVG} width={20} height={23}></img></div>}
-                        <div
-                            className={allResultsStyle['duration']}>{millisecondsToHhMmSs(Number(eachTrack?.duration_ms))}</div>
-                    </div>
-                </div>)
+                )
             }
-                else{
-                    return <TopSongCardSkeleton key={i}/>
-                }
-            })}
-
         </div>
     </div>
 }
--- front-end/src/skeletons/topSongCardSkeleton.tsx
+++ front-end/src/skeletons/topSongCardSkeleton.tsx
@@ -3,7 +3,27 @@ import skeletonStyle from "./skeletons.module.css";
 export function TopSongCardSkeleton(){
 
 
-    return <div></div>
+    return <div className={skeletonStyle['top-song-skeleton-wrapper']}>
+
+        <div className={skeletonStyle['top-song-details-skeleton']}>
+
+            <div className={skeletonStyle['top-song-image-skeleton']}></div>
+
+            <div className={skeletonStyle['top-song-title-artist-skeleton']}>
+                <div className={skeletonStyle['top-song-title-skeleton']}></div>
+                <div className={skeletonStyle['top-song-explicit-artists']}>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+
+        </div>
+
+        <div className={skeletonStyle['top-song-duration-skeleton']}>
+
+        </div>
+
+    </div>
 }
 
 export default TopSongCardSkeleton;
