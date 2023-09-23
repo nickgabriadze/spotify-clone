3f68a24abaad89c4cc8567b7cbe75aec75d6bd94
Nika Gabriadze <nickgabriadze@gmail.com>
2023-09-23 17:05:21 +0400

    formatted the api file

--- front-end/src/api/player/playResumeStreaming.ts
+++ front-end/src/api/player/playResumeStreaming.ts
@@ -1,35 +1,35 @@
 import axiosInstance from "../../axios"
 
-export async function PlayResumeStreaming(accessToken: string, context_uri?: string, track_uris?: string[] ){
-    if(context_uri){
-            return axiosInstance.put("/me/player/play", {
-                context_uri: context_uri
-            }, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                    "Content-Type": 'application/json'
-                }
-            })
-        }
-         if(track_uris){
-            return axiosInstance.put("/me/player/play", {
-                uris: track_uris
-            }, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                    "Content-Type": 'application/json'
-                }
-            })
-        }
-    
-            
-            return axiosInstance.put("/me/player/play", {},
-            {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-            })
-        
+export async function PlayResumeStreaming(accessToken: string, context_uri?: string, track_uris?: string[]) {
+    if (context_uri) {
+        return axiosInstance.put("/me/player/play", {
+            context_uri: context_uri
+        }, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": 'application/json'
+            }
+        })
+    }
+    if (track_uris) {
+        return axiosInstance.put("/me/player/play", {
+            uris: track_uris
+        }, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": 'application/json'
+            }
+        })
+    }
+
+
+    return axiosInstance.put("/me/player/play", {},
+        {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+
 
 }
 
