0d3d66cb39d60b54ccd362269ae6ed28648e604f
Nika Gabriadze <nickgabriadze@gmail.com>
2023-09-25 12:33:39 +0400

    moved navigation buttons(left-right) from the search, because it has to become the global one in order to display it for all the other components that will go inside the main section

--- front-end/src/components/main/Main.tsx
+++ front-end/src/components/main/Main.tsx
@@ -1,6 +1,9 @@
 import mainStyle from "./main.module.css";
 import Search from "../search/search.tsx";
 import {useAppSelector} from "../../store/hooks.ts";
+import searchBarStyle from "../search/components/search-bar/searchBar.module.css";
+import Left from "../search/components/search-bar/icons/left.svg";
+import Right from "../search/components/search-bar/icons/right.svg";
 
 
 export function Main({height}: { height: number }) {
@@ -9,8 +12,15 @@ export function Main({height}: { height: number }) {
 
     return (
         <main className={mainStyle['main-container']} style={{height: `${height}px`}}>
-
-            {navOption === 'search' ? <Search/> : <div></div>}
+                 <div className={searchBarStyle["left-right-nav"]}>
+                            <button>
+                                <img alt={'Left icon'} src={Left} height={32}></img>
+                            </button>
+                            <button style={{marginLeft: "-3px"}}>
+                                <img alt={'Right icon'} src={Right} height={32}></img>
+                            </button>
+                 </div>
+           <div style={{height: `${height}px`}}>{navOption === 'search' ? <Search/> : <div></div>}</div>
 
         </main>
 
