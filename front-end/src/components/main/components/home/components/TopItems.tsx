cb1143131b967dfe5e35abe5c0f6ed7b875cbbfc
Nika Gabriadze <nickgabriadze@gmail.com>
2023-10-06 14:54:06 +0400

    Added top items on the home page

--- front-end/src/components/main/components/home/components/TopItems.tsx
+++ front-end/src/components/main/components/home/components/TopItems.tsx
@@ -5,10 +5,12 @@ import getUsersTopItems from "../../../../../api/home/getUsersTopItems.ts";
 import {Artist} from "../../../../../types/artist.ts";
 import {Album} from "../../../../../types/album.ts";
 import {Track} from "../../../../../types/track.ts";
+import PlayButton from "../../../../search/components/each-search-component/Playlists/icons/play.svg";
 
 export function TopItems() {
     const [topItemsData, setTopItemsData] = useState<(Album | Artist)[]>([]);
     const access = useAppSelector((state) => state.spotiUserReducer.spotiToken.accessToken);
+    const [hoveringOverTopItem, setHoveringOverTopItem] = useState<{ itemID: string }>({itemID: ""});
 
     useEffect(() => {
         const fetchTops = async () => {
@@ -34,9 +36,28 @@ export function TopItems() {
         }
         fetchTops();
 
-
     }, [access])
-    return <div className={homepageStyle['user-top-items-wrapper']}></div>
+
+
+    return <div className={homepageStyle['user-top-items-wrapper']}>
+        {topItemsData.map((eachTopItem, i) => <div key={i} className={homepageStyle['top-item']}
+                                                   onMouseOver={() => setHoveringOverTopItem({itemID: eachTopItem.id})}
+                                                   onMouseOut={() => setHoveringOverTopItem({itemID: ''})}
+        >
+
+            <div className={homepageStyle['album-picture']}>
+                <img src={eachTopItem.images[0].url} alt={'Album Image'}></img>
+            </div>
+
+            <div className={homepageStyle['detail-play']}>
+                <div className={homepageStyle['top-item-title']}>{eachTopItem.name}</div>
+                {hoveringOverTopItem && hoveringOverTopItem.itemID === eachTopItem.id &&
+                    <div className={homepageStyle['play-button']}>
+                        <button><img alt={"Play"} draggable={false} src={PlayButton} height={50}></img></button>
+                    </div>}
+            </div>
+        </div>)}
+    </div>
 }
 
 export default TopItems;
