5ec43c906631b8dc879a0ffcd4312d677fceb06e
Nika Gabriadze <nickgabriadze@gmail.com>
2023-10-06 15:08:51 +0400

    made the top items functional in terms of streaming

--- front-end/src/components/main/components/home/components/TopItems.tsx
+++ front-end/src/components/main/components/home/components/TopItems.tsx
@@ -1,16 +1,21 @@
 import {useEffect, useState} from "react";
 import homepageStyle from "../homepage.module.css";
-import {useAppSelector} from "../../../../../store/hooks.ts";
+import {useAppDispatch, useAppSelector} from "../../../../../store/hooks.ts";
 import getUsersTopItems from "../../../../../api/home/getUsersTopItems.ts";
 import {Artist} from "../../../../../types/artist.ts";
 import {Album} from "../../../../../types/album.ts";
 import {Track} from "../../../../../types/track.ts";
-import PlayButton from "../../../../search/components/each-search-component/Playlists/icons/play.svg";
+import PlayResumeStreaming from "../../../../../api/player/playResumeStreaming.ts";
+import {setUserControlActions} from "../../../../../store/features/navigationSlice.ts";
+import PauseStreaming from "../../../../../api/player/pauseStreaming.ts";
+import Pause from "../../../../search/components/each-search-component/Playlists/icons/pause.svg";
+import Play from "../../../../search/components/each-search-component/Playlists/icons/play.svg";
 
 export function TopItems() {
     const [topItemsData, setTopItemsData] = useState<(Album | Artist)[]>([]);
     const access = useAppSelector((state) => state.spotiUserReducer.spotiToken.accessToken);
     const [hoveringOverTopItem, setHoveringOverTopItem] = useState<{ itemID: string }>({itemID: ""});
+    const currentlyPlaying = useAppSelector((state) => state.navigationReducer.currentlyPlayingSong);
 
     useEffect(() => {
         const fetchTops = async () => {
@@ -37,7 +42,7 @@ export function TopItems() {
         fetchTops();
 
     }, [access])
-
+    const dispatch = useAppDispatch();
 
     return <div className={homepageStyle['user-top-items-wrapper']}>
         {topItemsData.map((eachTopItem, i) => <div key={i} className={homepageStyle['top-item']}
@@ -53,7 +58,38 @@ export function TopItems() {
                 <div className={homepageStyle['top-item-title']}>{eachTopItem.name}</div>
                 {hoveringOverTopItem && hoveringOverTopItem.itemID === eachTopItem.id &&
                     <div className={homepageStyle['play-button']}>
-                        <button><img alt={"Play"} draggable={false} src={PlayButton} height={50}></img></button>
+                        <button
+                            onClick={async () => {
+                                if (!(currentlyPlaying.albumID === String(eachTopItem?.id) || currentlyPlaying.artistID === String(eachTopItem?.id))) {
+                                    await PlayResumeStreaming(access, eachTopItem.uri, undefined);
+                                    dispatch(
+                                        setUserControlActions({
+                                            userAction: "Play Track",
+                                        })
+                                    );
+                                } else if (currentlyPlaying.isPlaying && currentlyPlaying.albumID === String(eachTopItem?.id || currentlyPlaying.artistID === String(eachTopItem?.id))) {
+                                    await PauseStreaming(access);
+                                    dispatch(
+                                        setUserControlActions({
+                                            userAction: "Pause Track",
+                                        })
+                                    );
+                                } else if (!(currentlyPlaying.isPlaying) && currentlyPlaying.albumID === String(eachTopItem?.id) || currentlyPlaying.artistID === String(eachTopItem?.id)) {
+                                    await PlayResumeStreaming(access);
+                                    dispatch(
+                                        setUserControlActions({
+                                            userAction: "Resume Track",
+                                        })
+                                    );
+                                }
+                            }}
+                        >{(currentlyPlaying.albumID === String(eachTopItem?.id) || currentlyPlaying.artistID === String(eachTopItem?.id)) && currentlyPlaying.isPlaying === true ?
+                            <div><img src={Pause} width={30} height={30} alt={'Pause Button Image'}></img></div>
+                            :
+                            <div><img src={Play} width={50} height={50} alt={'Play Button Image'}></img></div>
+                        }
+                        </button>
+
                     </div>}
             </div>
         </div>)}
