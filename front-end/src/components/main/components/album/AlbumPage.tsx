b576d62d48249f7b75afeac492dfd4d50bc2c89a
Nika Gabriadze <nickgabriadze@gmail.com>
2023-10-14 13:31:44 +0400

    Started working on album page

---
+++ front-end/src/components/main/components/album/AlbumPage.tsx
@@ -0,0 +1,10 @@
+import {Album} from "../../../../types/album.ts";
+
+export function AlbumPage({albumData}:{albumData: Album}){
+
+
+
+    return <>This is album page</>
+}
+
+export default AlbumPage;
