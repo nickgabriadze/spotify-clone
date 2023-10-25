f8d9abf90a86ed214d5bfc68e8fab01609fd343a
Nika Gabriadze <nickgabriadze@gmail.com>
2023-10-25 13:19:32 +0400

    Added top songs on Artist's page and fixed a few issues that came up while fixing the css for these popular tracks.

--- front-end/src/components/main/components/artist/ArtistPage.tsx
+++ front-end/src/components/main/components/artist/ArtistPage.tsx
@@ -10,6 +10,9 @@ import Pause from "../../../search/components/each-search-component/Playlists/ic
 import Play from "../../../search/components/each-search-component/Playlists/icons/play.svg";
 import removeArtistForCurrentUser from "../../../../api/library/removeArtistForCurrentUser.ts";
 import saveArtistForCurrentUser from "../../../../api/library/saveArtistForCurrentUser.ts";
+import getArtistsPopularTracks from "../../../../api/main/artist/getArtistsPopularTracks.ts";
+import {Track} from "../../../../types/track.ts";
+import {SongCard} from "../../../search/reuseables/songCard.tsx";
 
 export function ArtistPage({artistID}: { artistID: string }) {
     const [artistData, setArtistData] = useState<Artist>();
@@ -17,25 +20,28 @@ export function ArtistPage({artistID}: { artistID: string }) {
     const dispatch = useAppDispatch();
     const isArtistSaved = useAppSelector(s => s.spotiUserReducer.userSaved.userSavedArtistIDs).includes(artistID)
     const currentlyPlaying = useAppSelector(s => s.navigationReducer.currentlyPlayingSong)
-
+    const country = String(useAppSelector(s => s.spotiUserReducer.userInformation?.country));
+    const [artistTopTracks, setArtistTopTracks] = useState<Track[]>([])
+    const [showMore, setShowMore] = useState<boolean>(false)
     useEffect(() => {
         const fetchArtist = async () => {
             try {
                 const artistData = (await getArtist(accessToken, artistID)).data;
                 setArtistData(artistData)
-
+                const topTracks = (await getArtistsPopularTracks(accessToken, artistID, country)).data.tracks
+                setArtistTopTracks(topTracks)
             } catch (err) {
 
             }
         }
         fetchArtist();
-    }, [artistID, accessToken]);
+    }, [artistID, accessToken, country]);
 
     return <section className={artistPageStyle['artist-page-wrapper']}>
 
         <div className={artistPageStyle['artist-general-info']}>
             <div className={artistPageStyle['artist-image']}><img alt={"Artists Image"} draggable={false}
-                                                                  src={artistData?.images[0].url}></img></div>
+                                                                  src={artistData?.images[0]?.url}></img></div>
             <div className={artistPageStyle['artist-name']}>
                 <h1>{artistData?.name.slice(0, 25)}</h1>
                 <p>{artistData?.followers.total.toLocaleString()} followers</p>
@@ -111,6 +117,22 @@ export function ArtistPage({artistID}: { artistID: string }) {
             </div>
         </div>
 
+        <div className={artistPageStyle['top-tracks']}>
+            <h1>Popular</h1>
+
+            <div className={artistPageStyle['tracks-box']}>
+                <div className={artistPageStyle['tracks']}>
+                    {artistTopTracks.length > 0 && artistTopTracks.slice(0, showMore ? 10 : 5).map((track, i) =>
+                        <SongCard eachTrack={track} n={i + 1}
+                                  accessToken={accessToken}
+                                  forAlbum={false}
+                                  forArtist={true}/>)}
+                </div>
+                <button className={artistPageStyle['see-more-tracks']}
+                        onClick={() => setShowMore(prev => !prev)}
+                ><p>{showMore ? 'Show less' : 'See more'}</p></button>
+            </div>
+        </div>
     </section>
 }
 
--- front-end/src/components/search/components/each-search-component/Songs/Songs.tsx
+++ front-end/src/components/search/components/each-search-component/Songs/Songs.tsx
@@ -1,163 +1,167 @@
 import Duration from "../icons/duration.svg";
 import songsStyle from "./songs.module.css";
-import { useState, useEffect, useRef, useCallback } from "react";
-import { Track, Tracks } from "../../../../../types/track";
-import { useAppSelector } from "../../../../../store/hooks";
+import {useState, useEffect, useRef, useCallback} from "react";
+import {Track, Tracks} from "../../../../../types/track";
+import {useAppSelector} from "../../../../../store/hooks";
 import getTracks from "../../../../../api/search/getTracks";
-import { SongCard } from "../../../reuseables/songCard";
+import {SongCard} from "../../../reuseables/songCard";
 import SongCardSkeleton from "../../../../../skeletons/songCardSkeleton";
-export function SongsRes({ songName }: { songName: string }) {
-  const [tracksData, setTracksData] = useState<{
-    data: Array<Track | typeof SongCardSkeleton>;
-    next: string | null;
-  }>({
-    next: null,
-    data: [],
-  });
-  const [tracksLoading, setTracksLoading] = useState<boolean>(true);
-  const [_tracksError, setTracksError] = useState<string | unknown>();
-  const query = songName.split(" ").join("%20");
-  const observing = useRef<null | IntersectionObserver>(null);
 
-  const accessToken = useAppSelector(
-    (state) => state.spotiUserReducer.spotiToken.accessToken
-  );
-
-  useEffect(() => {
-    setTracksData({
-      data: [],
-      next: null,
+export function SongsRes({songName}: { songName: string }) {
+    const [tracksData, setTracksData] = useState<{
+        data: Array<Track | typeof SongCardSkeleton>;
+        next: string | null;
+    }>({
+        next: null,
+        data: [],
     });
-  }, [songName]);
+    const [tracksLoading, setTracksLoading] = useState<boolean>(true);
+    const [_tracksError, setTracksError] = useState<string | unknown>();
+    const query = songName.split(" ").join("%20");
+    const observing = useRef<null | IntersectionObserver>(null);
 
-  useEffect(() => {
-    const fetchTracks = async () => {
-      setTracksData((prev) => {
-        return {
-          ...prev,
-          data: [...prev.data, ...Array(30).fill(SongCardSkeleton)],
-          next: prev.next,
-        };
-      });
-      try {
-        setTracksLoading(true);
-        const req = await getTracks(
-          `/search?q=${query}&type=track&limit=30`,
-          accessToken
-        );
-        const data: Tracks = req.data.tracks;
+    const accessToken = useAppSelector(
+        (state) => state.spotiUserReducer.spotiToken.accessToken
+    );
 
-        setTracksData((prev) => {
-          return {
-            ...prev,
-            data: [
-              ...prev.data.filter((e) => typeof e !== typeof SongCardSkeleton),
-              ...data.items,
-            ],
-            next: data.next,
-          };
+    useEffect(() => {
+        setTracksData({
+            data: [],
+            next: null,
         });
-      } catch (err) {
-        setTracksError(err);
-      } finally {
-        setTracksLoading(false);
-      }
-    };
+    }, [songName]);
 
-    fetchTracks();
-  }, [accessToken, query]);
+    useEffect(() => {
+        const fetchTracks = async () => {
+            setTracksData((prev) => {
+                return {
+                    ...prev,
+                    data: [...prev.data, ...Array(30).fill(SongCardSkeleton)],
+                    next: prev.next,
+                };
+            });
+            try {
+                setTracksLoading(true);
+                const req = await getTracks(
+                    `/search?q=${query}&type=track&limit=30`,
+                    accessToken
+                );
+                const data: Tracks = req.data.tracks;
 
-  const lastSong = useCallback(
-    (node: HTMLDivElement | null) => {
-      const fetchTracks = async () => {
-        try {
-          setTracksData((prev) => {
-            return {
-              ...prev,
-              data: [...prev.data, ...Array(30).fill(SongCardSkeleton)],
-              next: prev.next,
+                setTracksData((prev) => {
+                    return {
+                        ...prev,
+                        data: [
+                            ...prev.data.filter((e) => typeof e !== typeof SongCardSkeleton),
+                            ...data.items,
+                        ],
+                        next: data.next,
+                    };
+                });
+            } catch (err) {
+                setTracksError(err);
+            } finally {
+                setTracksLoading(false);
+            }
+        };
+
+        fetchTracks();
+    }, [accessToken, query]);
+
+    const lastSong = useCallback(
+        (node: HTMLDivElement | null) => {
+            const fetchTracks = async () => {
+                try {
+                    setTracksData((prev) => {
+                        return {
+                            ...prev,
+                            data: [...prev.data, ...Array(30).fill(SongCardSkeleton)],
+                            next: prev.next,
+                        };
+                    });
+                    setTracksLoading(true);
+                    const req = await getTracks(
+                        tracksData.next
+                            ? tracksData.next
+                            : `/search?q=${query}&type=track&limit=30`,
+                        accessToken
+                    );
+                    const data: Tracks = req.data.tracks;
+
+                    setTracksData((prev) => {
+                        return {
+                            ...prev,
+                            data: [...prev.data.filter((e) => typeof e !== typeof SongCardSkeleton), ...data.items],
+                            next: data.next,
+                        };
+                    });
+                } catch (err) {
+                    setTracksError(err);
+                } finally {
+                    setTracksLoading(false);
+                }
             };
-          });
-          setTracksLoading(true);
-          const req = await getTracks(
-            tracksData.next
-              ? tracksData.next
-              : `/search?q=${query}&type=track&limit=30`,
-            accessToken
-          );
-          const data: Tracks = req.data.tracks;
 
-          setTracksData((prev) => {
-            return {
-              ...prev,
-              data: [...prev.data.filter((e) => typeof e !== typeof SongCardSkeleton), ...data.items],
-              next: data.next,
-            };
-          });
-        } catch (err) {
-          setTracksError(err);
-        } finally {
-          setTracksLoading(false);
-        }
-      };
-     
-      if (!node) return;
-      if (tracksLoading) return;
-      if (observing.current) observing.current?.disconnect();
-      observing.current = new IntersectionObserver((e) => {
-        if (e[0].isIntersecting) {
-       
-          fetchTracks();
-        }
-      });
-      observing.current?.observe(node);
-    },
-    [accessToken, query, tracksData.next, tracksLoading]
-  );
+            if (!node) return;
+            if (tracksLoading) return;
+            if (observing.current) observing.current?.disconnect();
+            observing.current = new IntersectionObserver((e) => {
+                if (e[0].isIntersecting) {
 
-  return (
-    <section className={songsStyle["tracks-wrapper"]}>
-      <div className={songsStyle["navigation-wrapper"]}>
-        <div className={songsStyle["numbering-title"]}>
-          <div>#</div>
-          <div>Title</div>
-        </div>
+                    fetchTracks();
+                }
+            });
+            observing.current?.observe(node);
+        },
+        [accessToken, query, tracksData.next, tracksLoading]
+    );
 
-        <div>Album</div>
+    return (
+        <section className={songsStyle["tracks-wrapper"]}>
+            <div className={songsStyle["navigation-wrapper"]}>
+                <div className={songsStyle["numbering-title"]}
 
-        <div>
-          <img
-            src={Duration}
-            draggable={false}
-            width={20}
-            height={20}
-            alt="Duration"
-            style={{ marginBottom: "-5px" }}
-          ></img>
-        </div>
-      </div>
+                >
+                    <div>#</div>
+                    <div>Title</div>
+                </div>
 
-      <div>
-        {tracksData.data.map((eachTrack, i) => {
-          if (typeof eachTrack === typeof SongCardSkeleton) {
-            return <SongCardSkeleton key={i} />;
-          } else {
-            return (
-              
-              <SongCard
-                ref={i === tracksData.data.length - 1 ? lastSong : null}
-                n={i + 1}
-                key={i}
-                forAlbum={false}
-                accessToken={accessToken}
-                eachTrack={typeof eachTrack === "object" ? eachTrack : undefined}
-              />
-            );
-          }
-        })}
-      </div>
-    </section>
-  );
+                <div>Album</div>
+
+                <div>
+                    <img
+                        src={Duration}
+                        draggable={false}
+                        width={20}
+                        height={20}
+                        alt="Duration"
+                        style={{marginBottom: "-5px"}}
+                    ></img>
+                </div>
+            </div>
+
+            <div>
+                {tracksData.data.map((eachTrack, i) => {
+                    if (typeof eachTrack === typeof SongCardSkeleton) {
+                        return <SongCardSkeleton key={i}/>;
+                    } else {
+                        return (
+
+                            <SongCard
+                                ref={i === tracksData.data.length - 1 ? lastSong : null}
+                                n={i + 1}
+                                key={i}
+                                forAlbum={false}
+                                forArtist={false}
+                                accessToken={accessToken}
+                                eachTrack={typeof eachTrack === "object" ? eachTrack : undefined}
+                            />
+                        );
+                    }
+                })}
+            </div>
+        </section>
+    );
 }
 
 export default SongsRes;
