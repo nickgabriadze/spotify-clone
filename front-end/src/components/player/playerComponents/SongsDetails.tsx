1dd36ffdfd2b006770d8dad82db4584790305ab4
Nika Gabriadze <nickgabriadze@gmail.com>
2023-11-09 16:59:26 +0400

    fixed small song detail 'name' width issue

--- front-end/src/components/player/playerComponents/SongsDetails.tsx
+++ front-end/src/components/player/playerComponents/SongsDetails.tsx
@@ -33,7 +33,7 @@ export function SongDetails({currentlyPlaying}: { currentlyPlaying: CurrentlyPla
     useEffect(() => {
         if (songNameHover) {
 
-            if (Number(songNameRef?.current?.offsetWidth) > 160) {
+            if (Number(songNameRef?.current?.offsetWidth) > 140) {
                 songNameRef?.current?.classList.add('song-name-animation');
             } else {
                 songNameRef?.current?.classList.remove('song-name-animation');
@@ -81,7 +81,7 @@ export function SongDetails({currentlyPlaying}: { currentlyPlaying: CurrentlyPla
 
         >
             <div className={playerStyle['song-info-box-without-heart']}
-                 style={Number(songNameRef?.current?.offsetWidth) > 160
+                 style={Number(songNameRef?.current?.offsetWidth) > 140
                  || Number(artistNameRef?.current?.children.length) > 2
                      ? {boxShadow: `-10px 0 27px -26px #B3B3B3  inset`, width: '200px'} : {width: 'fit-content'}
                  }
