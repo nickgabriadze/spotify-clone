1bab0f9401a48d3c2cdb391e8980952dc9d8d66e
Nika Gabriadze <nickgabriadze@gmail.com>
2023-12-30 14:21:31 +0400

    style: added small styling to searchError component.

--- front-end/src/components/Errors/SearchError.tsx
+++ front-end/src/components/Errors/SearchError.tsx
@@ -1,10 +1,31 @@
-export function SearchError(){
+import {useParams} from "react-router-dom";
 
+export function SearchError() {
 
-    return <div>
-        <h1>No results found for ...</h1>
+    const {query} = useParams();
 
-        <p>Please make sure your words are spelled correctly or try reloading the page.</p>
+    return <div style={{
+        'display': 'flex',
+        'justifyContent': 'center',
+        'alignItems': 'center',
+        'width': '100%',
+        'flexDirection': 'column',
+        'color': 'white',
+        'padding': '10px',
+        'gap': '5px',
+        fontSize: '1.2rem'
+
+    }}>
+        <h1>No results found for</h1>
+        <h5 style={{
+            wordBreak: 'break-all',
+        }}>{query}</h5>
+        <h1
+            style={{
+                fontFamily: 'Gotham Medium, sans-serif',
+                fontWeight: 'bold',
+            }}
+        >Please make sure your words are spelled correctly or try reloading the page.</h1>
     </div>
 
 }
