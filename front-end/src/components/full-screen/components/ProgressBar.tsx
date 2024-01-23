8708c87c56eaf16c3e721f9c2165d4ca63bdc9fb
Nick <nickgabriadze@gmail.com>
2024-01-23 17:27:15 +0400

    feat: made trackbar responsive to clicks(will add actual "skip to point" later)

--- front-end/src/components/full-screen/components/ProgressBar.tsx
+++ front-end/src/components/full-screen/components/ProgressBar.tsx
@@ -1,17 +1,19 @@
 import FSComponentStyle from './fs.module.css';
 import {useAppSelector} from "../../../store/hooks.ts";
 import millisecondsToHhMmSs from "../../player/msConverter.ts";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 export function ProgressBar() {
     const currentlyPlayingTrack = useAppSelector(s => s.navigationReducer.currentSongData)
-    const [trackProgressState, setTrackProgressState] = useState<number>(Number(currentlyPlayingTrack?.progress_ms))
-    const trackProgress = millisecondsToHhMmSs(trackProgressState, false)
-    const totalDuration = millisecondsToHhMmSs(Number(currentlyPlayingTrack?.item.duration_ms) - trackProgressState, false)
+    const [trackProgressState, setTrackProgressState] = useState<number[]>([(Number(currentlyPlayingTrack?.progress_ms) / Number(currentlyPlayingTrack?.item?.duration_ms)) * 100, Number(currentlyPlayingTrack?.progress_ms)])
+    const trackProgress = millisecondsToHhMmSs(trackProgressState[1], false)
+    const totalDuration = millisecondsToHhMmSs(Number(currentlyPlayingTrack?.item.duration_ms) - trackProgressState[1], false)
     const [hoveringOverTrackbar, setHoveringOverTrackbar] = useState<boolean>(false)
+    const trackBarProgressRef = useRef<HTMLDivElement | null>(null)
+    const trackInnerProgressRef = useRef<HTMLDivElement | null>(null)
     useEffect(() => {
 
-        setTrackProgressState(Number(currentlyPlayingTrack?.progress_ms))
+        setTrackProgressState((prev) => [(prev[1] / Number(currentlyPlayingTrack?.item?.duration_ms)) * 100, Number(currentlyPlayingTrack?.progress_ms)])
 
     }, [currentlyPlayingTrack?.progress_ms, currentlyPlayingTrack?.item.uri]);
 
@@ -19,26 +21,50 @@ export function ProgressBar() {
 
         const interval = setInterval(() => {
             if (currentlyPlayingTrack?.is_playing && currentlyPlayingTrack?.progress_ms <= currentlyPlayingTrack?.item.duration_ms) {
-                setTrackProgressState((prev) => prev + 1000)
+                setTrackProgressState((prev) => [(prev[1] / Number(currentlyPlayingTrack?.item?.duration_ms)) * 100, prev[1] + 1000])
             }
         }, 1000)
 
         return () => clearInterval(interval)
 
-    }, [trackProgressState, currentlyPlayingTrack?.item?.duration_ms, currentlyPlayingTrack?.is_playing]);
+    }, [trackProgressState[1], currentlyPlayingTrack?.item?.duration_ms, currentlyPlayingTrack?.is_playing]);
+
+
+    useEffect(() => {
+        const trackMouse = (e: MouseEvent) => {
+            if (e.target === trackBarProgressRef?.current
+                || e.target === trackInnerProgressRef?.current
+            ) {
+                const clientClickedOnX = (e.clientX - Number(trackBarProgressRef.current?.getClientRects()[0].x) - 5) / Number(trackBarProgressRef?.current?.clientWidth)
+                console.log(clientClickedOnX)
+
+                setTrackProgressState([clientClickedOnX * 100, clientClickedOnX * Number(currentlyPlayingTrack?.item.duration_ms)])
+
+
+            }
+
+        }
+
+        window.addEventListener('click', (e) => trackMouse(e))
+
+
+        return () => window.removeEventListener('click', (e) => trackMouse(e))
+    }, [trackBarProgressRef?.current, trackInnerProgressRef?.current])
 
 
     return currentlyPlayingTrack && <div className={FSComponentStyle['progress-bar']}>
         <div className={FSComponentStyle['time-s-e']}>{trackProgress}</div>
 
         <div className={FSComponentStyle['outer-progress-tracker']}
+             ref={trackBarProgressRef}
              onMouseOver={() => setHoveringOverTrackbar(true)}
              onMouseOut={() => setHoveringOverTrackbar(false)}
         >
             <div className={FSComponentStyle['inner-progress-tracker']}
+                 ref={trackInnerProgressRef}
 
                  style={{
-                     width: `${(trackProgressState / Number(currentlyPlayingTrack?.item.duration_ms)) * 100}%`,
+                     width: `${trackProgressState[0]}%`,
                      cursor: 'pointer',
                      backgroundColor: hoveringOverTrackbar ? '#1ed760' : 'white'
                  }}
